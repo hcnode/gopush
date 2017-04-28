afcbb364f68d1c99beb8792655120eab4febf8c7
hcnode <7372@163.com>
2017-04-28 15:30:01 +0800

    update

--- hook/onupgrade-middleware_production.js
+++ hook/onupgrade-middleware_production.js
@@ -1,7 +1,15 @@
-var parseCookies = require('../tools/parseCookies');
 module.exports = function(req){
 	return new Promise((resolve, reject) => {
         var cookies = parseCookies(req);
         resolve(cookies.uid);
     });
+}
+function parseCookies (request) {
+    var list = {},
+        rc = request.headers.cookie;
+    rc && rc.split(';').forEach(function( cookie ) {
+        var parts = cookie.split('=');
+        list[parts.shift().trim()] = (parts.join('='));
+    });
+    return list;
 }
